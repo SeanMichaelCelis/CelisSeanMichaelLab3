4a61716546e262f6cf44b0e435203074bf5af7d7
SeanMichaelCelis <2015081567@ust-ics.mygbiz.com>
2018-09-07 09:14:59 -0400

    Initial commit

---
+++ app/src/main/java/sean/celis/com/celisseanmichaellab3/StylesAndMaterialDesign.java
@@ -0,0 +1,13 @@
+package sean.celis.com.celisseanmichaellab3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StylesAndMaterialDesign extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_styles_and_material_design);
+    }
+}
